3d2a39d6d69d37ee1e6ee315f859c02f74e17c51
RiadMohamed <riaduk1@gmail.com>
2018-12-07 13:29:49 +0200

    Initial Commit

---
+++ Chess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by Riad Mohamed on 12/7/18.
+//  Copyright © 2018 Riad Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
